218340946ed5ae90fd7be79b87ed6c9f83ed661c
Mattie Niznik <mattie.niznik@princeton.edu>
2025-10-17 00:05:48 -0400

    Add reimagined setup_inspect.py

--- tools/inspect/setup_inspect.py
+++ tools/inspect/setup_inspect.py
@@ -1,37 +1,31 @@
+import argparse
 import os
 import shutil
 import sys
-import argparse
+from pathlib import Path
 
-# If possible, see if the input file comes from one of our test directories and if so grab the inspect.py from there
+# Calculate paths relative to this script
+script_dir = Path(__file__).parent
+
+# If possible, see if the input file comes from one of our task directories and if so grab the inspect.py from there
 parser = argparse.ArgumentParser(description="Set up the inspect.py file for the current project.")
-parser.add_argument("source_dir", type=str, required=True, help="Path to the directory containing inspect.py")
+parser.add_argument("--base_model_dir", type=str, default="", help="Path to a base model directory")
+parser.add_argument("--finetune_epoch_dir", type=str, required=True, help="Path to a finetuned model's target epoch (and its slurm parameters one folder up); if base_model_dir is provided, only the slurm parameters are used")
 args = parser.parse_args()
 
-source_file = os.path.join(args.source_dir, "inspect.py")
-destination_file = "inspect.py"
-
-if not os.path.isfile(source_file):
-    print(f"Error: {source_file} does not exist.")
-    sys.exit(1)
-
-shutil.copy(source_file, destination_file)
-print(f"Copied {source_file} to {destination_file}.")
-
-# If we don't know where the input data came from, warn the user they'll have to supply their own inspect.py
-
-# OK now we can parse the slurm script and make minimal changes
-# A list of those would be: match nodes/ntasks/cpus-per-task/mem/time to the model output's slurm params (do we have those?)
-# Allow account/partition/constraint to be changed
-# Update conda env name if needed
-# Update output dir
-# Otherwise this one is much simpler than the finetune one. We could have this generate the inspect.py but that's a lot more work
-# So for now we just copy the file and make minimal changes to the slurm script
-# For now, just copy the template slurm script and tell the user to edit it
-
-with open("templates/inspect_template.slurm", "r") as f:
+# Decide where we'll load the model from
+MODEL_PATH = args.base_model_dir if args.base_model_dir else args.finetune_epoch_dir
+with open(f"{args.finetune_epoch_dir}/../finetune.slurm", "r") as f:
     slurm_script = f.read()
 
-# TODO - I'll work on this later
+slurm_script = slurm_script.replace(" # Job name", "-i # Job name")
+
+# Remove all lines after the conda environment is activated
+slurm_script = slurm_script.split("mkdir -p")[0]
+
+slurm_script += f"inspect eval inspect.py --model hf/local -M model_path={MODEL_PATH} -T config_dir={args.finetune_epoch_dir}"
+
+with open("inspect.slurm", "w") as f:
+    f.write(slurm_script)
 
 print("Ready to inspect with `sbatch inspect.slurm` !")
