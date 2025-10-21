521491b2570ecd227e33d8c1c345227f7a1f6ae0
Mattie Niznik <mattie.niznik@princeton.edu>
2025-10-21 13:23:48 -0400

    Update python script inspect.py to run_inspect.py to avoid collisions

--- tools/inspect/setup_inspect.py
+++ tools/inspect/setup_inspect.py
@@ -7,8 +7,7 @@ from pathlib import Path
 # Calculate paths relative to this script
 script_dir = Path(__file__).parent
 
-# If possible, see if the input file comes from one of our task directories and if so grab the inspect.py from there
-parser = argparse.ArgumentParser(description="Set up the inspect.py file for the current project.")
+parser = argparse.ArgumentParser(description="Set up the run_inspect.py file for the current project.")
 parser.add_argument("--base_model_dir", type=str, default="", help="Path to a base model directory")
 parser.add_argument("--finetune_epoch_dir", type=str, required=True, help="Path to a finetuned model's target epoch (and its slurm parameters one folder up); if base_model_dir is provided, only the slurm parameters are used")
 args = parser.parse_args()
@@ -23,7 +22,7 @@ slurm_script = slurm_script.replace(" # Job name", "-i # Job name")
 # Remove all lines after the conda environment is activated
 slurm_script = slurm_script.split("mkdir -p")[0]
 
-slurm_script += f"inspect eval inspect.py --model hf/local -M model_path={MODEL_PATH} -T config_dir={args.finetune_epoch_dir}"
+slurm_script += f"inspect eval run_inspect.py --model hf/local -M model_path={MODEL_PATH} -T config_dir={args.finetune_epoch_dir}"
 
 with open("inspect.slurm", "w") as f:
     f.write(slurm_script)
